3cc87538379349e5cb76675241e490dc18fcae38
mikegroat <michael.j.groat@gmail.com>
2018-03-19 17:20:08 -0400

    Updated color.js to manage multiple color palettes

--- color.js
+++ color.js
@@ -1,13 +1,30 @@
-// A library for colors
-// Set the colors in the colorArray, using a color wheel program (like Adobe Kule)
-var colorArray = [
-    '#FF3924',
-    '#BF2B1B',
-    '#7F1D12',
-    '#400E09'
-];
+    // A library for colors
+    // Set the colors in the colorArray, using a color wheel program (like Adobe Kuler)
+    // Each palette is an object with the following structure:
+    // { name: "name", colors: [array of colors]}
+function Palettes() {
 
-function randomColor(colors) {
-    var i = randIntBetween(0, colors.length - 1);
-    return colors[i];
+    this.palettes = [];
+
+    this.createPalette = function(palette) {
+        if (name in palette & colors in palette) {
+            this.palettes.push(palette);
+        }
+    }
+
+    this.randomPalette = function() {
+        return palettes[randIntBetween(0, this.palettes.length)];
+    }
+
+    this.randomColor = function(palette) {
+        var i = randIntBetween(0, palette.colors.length - 1);
+        return palette[i];
+    }
+
+    this.getPalette = function(name) {
+        return palettes.find(function(element) {
+            return element.name == name; 
+        });
+    }
+
 }
